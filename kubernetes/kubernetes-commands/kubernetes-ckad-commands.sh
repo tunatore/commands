269ad1e7e81ba20f32129b6f61210177afd008d9
tuna <tunatore@gmail.com>
2020-07-10 16:02:35 +0200

    kubernetes ckad commands updates

--- kubernetes/kubernetes-commands/kubernetes-ckad-commands.sh
+++ kubernetes/kubernetes-commands/kubernetes-ckad-commands.sh
@@ -1,5 +1,5 @@
-#context
-
+context
+-----------------------------------------------------------------------
 kubectl config get-contexts
 CURRENT   NAME                 CLUSTER          AUTHINFO         NAMESPACE
           docker-desktop       docker-desktop   docker-desktop
@@ -12,22 +12,46 @@ minikube
 kubectl config use-context minikube
 Switched to context "minikube".
 
-#namespaces
+namespaces
+-----------------------------------------------------------------------
 -create namespace
 kubectl create namespace mynamespace
+
 -get all pods in a namespace
 kubectl get pods --all-namespaces
 or
 kubectl get pods -A
 
-#pod
+pod
+-----------------------------------------------------------------------
 -create pod help command
 kubectl create pod -h
+
 -create a pod in the namespace
 kubectl run nginx --image=nginx --restart=Never -n mynamespace
+
 -create a pod from yaml
 kubectl create -f pod.yaml
+
 -delete a pod
 kubectl delete pod podname
+
 -show all labels
 kubectl get pods --show-labels
+
+-export to yaml
+kubectl get pod podname -o yaml > pod.yaml
+
+-label a pod
+kubectl label pod podname env=qa
+kubectl label pod podname env=prod --overwrite
+
+deployment
+-----------------------------------------------------------------------
+kubectl get deploy
+kubectl get deployment deploymentname -o yaml > deployment.yaml
+
+service
+-----------------------------------------------------------------------
+kubectl get service
+kubectl get service servicename -o yaml > service.yaml
