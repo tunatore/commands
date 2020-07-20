56fd7af08930d867af6ad6bec74c10b51c82872e
tuna <tunatore@gmail.com>
2020-07-20 18:45:14 +0200

    kubernetes network policy

--- kubernetes/ckad/ckad-commands.sh
+++ kubernetes/ckad/ckad-commands.sh
@@ -300,3 +300,338 @@ spec:
 
 k -f pod1.yaml replace --force --grace-period 0
 k exec pod1 env
+
+Network Policy
+k config get-contexts
+k config set-context minikube
+
+vi nginx-networkpolicy.yaml
+apiVersion: networking.k8s.io/v1
+kind: NetworkPolicy
+metadata:
+  name: nginx-network-policy
+spec:
+  podSelector:
+    matchLabels:
+      app: nginx
+  policyTypes:
+  - Egress
+  egress:
+  - to:
+    - podSelector:
+        matchLabels:
+          app: api
+    ports:
+      - port: 3333
+        protocol: TCP
+  - to:
+    ports:
+      - port: 53
+        protocol: TCP
+      - port: 53
+        protocol: UDP
+
+k create -f nginx-networkpolicy.yaml
+
+vi api-networkpolicy.yaml
+apiVersion: networking.k8s.io/v1
+kind: NetworkPolicy
+metadata:
+  name: api-network-policy
+spec:
+  podSelector:
+    matchLabels:
+      app: api
+  policyTypes:
+  - Ingress
+  ingress:
+    - from:
+      - podSelector:
+          matchLabels:
+            app: nginx
+      ports:
+        - port: 3333
+          protocol: TCP
+
+k create -f api-networkpolicy.yaml
+
+vi api-networkpolicy.yaml
+apiVersion: networking.k8s.io/v1
+kind: NetworkPolicy
+metadata:
+  name: api-network-policy
+spec:
+  podSelector:
+    matchLabels:
+      app: api
+  policyTypes:
+  - Egress
+  - Ingress
+  ingress:
+    - from:
+      - podSelector:
+          matchLabels:
+            app: nginx
+      ports:
+        - port: 3333
+          protocol: TCP
+  egress:
+  - to:
+    - ipBlock:
+        cidr: 216.58.208.35/32
+    ports:
+      - port: 443
+        protocol: TCP
+  - to:
+    ports:
+      - port: 53
+        protocol: TCP
+      - port: 53
+        protocol: UDP
+
+k apply -f api-networkpolicy.yaml
+
+Logging Sidecar
+vi deployment.yaml
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  creationTimestamp: null
+  labels:
+    run: nginx
+  name: nginx
+spec:
+  replicas: 1
+  selector:
+    matchLabels:
+      run: nginx
+  strategy: {}
+  template:
+    metadata:
+      creationTimestamp: null
+      labels:
+        run: nginx
+    spec:
+      volumes:
+      - name: logs
+        emptyDir: {}
+      containers:
+      - image: nginx
+        name: nginx
+        resources: {}
+        volumeMounts:
+          - name: logs
+            mountPath: /var/log/nginx
+status: {}
+
+k create -f deployment.yaml
+
+vi loadbalancer.yaml
+apiVersion: v1
+kind: Service
+metadata:
+  creationTimestamp: null
+  labels:
+    run: nginx
+  name: nginx
+spec:
+  ports:
+  - port: 1234
+    protocol: TCP
+    targetPort: 80
+  selector:
+    run: nginx
+  type: LoadBalancer
+status:
+  loadBalancer: {}
+
+k create -f loadbalancer.yaml
+
+k edit deploy nginx
+
+k nginx -o yaml --export > d_nginx.yaml
+
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  creationTimestamp: null
+  labels:
+    run: nginx
+  name: nginx
+spec:
+  replicas: 1
+  selector:
+    matchLabels:
+      run: nginx
+  strategy: {}
+  template:
+    metadata:
+      creationTimestamp: null
+      labels:
+        run: nginx
+    spec:
+      volumes:
+      - name: logs
+        emptyDir: {}
+      containers:
+      - image: bash
+        name: sidecar
+        volumeMounts:
+        - mountPath: /tmp/logs
+          name: logs
+        command:
+          - "/bin/sh"
+          - "-c"
+          - "tail -f /tmp/logs/access.log"
+      - image: nginx
+        name: nginx
+        resources: {}
+        volumeMounts:
+          - name: logs
+            mountPath: /var/log/nginx
+status: {}
+
+k replace deploy -f deployment.yaml
+k delete deploy nginx
+
+k logs nginx-6b48fb65c6-mhm8t sidecar
+
+ReplicaSet without Downtime
+vi pod.yaml
+apiVersion: v1
+kind: Pod
+metadata:
+  name: pod-calc
+spec:
+  containers:
+  - command:
+    - sh
+    - -c
+    - echo "important calculation"; sleep 1d
+    image: nginx
+    name: pod-calc
+
+k create -f pod.yaml
+
+k label pod pod-calc id=calc
+
+k get pod --show-labels
+
+vi replicaset.yaml
+apiVersion: apps/v1
+kind: ReplicaSet
+metadata:
+  name: rs1
+spec:
+  replicas: 2
+  selector:
+    matchLabels:
+      id: calc
+  template:
+    # from here down its the pod template
+    metadata:
+      name: pod-calc
+      labels:
+        id: calc
+    spec:
+      containers:
+      - command:
+        - sh
+        - -c
+        - echo "important calculation"; sleep 1d
+        image: nginx
+        name: pod-calc
+
+k create -f replicaset.yaml
+
+Various Env Variables
+vi env-variables.env
+CREDENTIAL_001=-bQ(ETLPGE[uT?6C;ed
+CREDENTIAL_002=C_;SU@ev7yg.8m6hNqS
+CREDENTIAL_003=ZA#$$-Ml6et&4?pKdvy
+CREDENTIAL_004=QlIc3$5*+SKsw==9=p{
+CREDENTIAL_005=C_2\a{]XD}1#9BpE[k?
+CREDENTIAL_006=9*KD8_w<);ozb:ns;JC
+CREDENTIAL_007=C[V$Eb5yQ)c~!..{LRT
+SETTING_USE_SEC=true
+SETTING_ALLOW_ANON=true
+SETTING_PREVENT_ADMIN_LOGIN=true
+
+k create secret -h
+k create secret generic -h
+k create secret generic my-secret --from-env-file=env_file
+k get secret my-secret-env -oyaml
+
+apiVersion: v1
+data:
+  CREDENTIAL_001: LWJRKEVUTFBHRVt1VD82QztlZA==
+  CREDENTIAL_002: Q187U1VAZXY3eWcuOG02aE5xUw==
+  CREDENTIAL_003: WkEjJCQtTWw2ZXQmND9wS2R2eQ==
+  CREDENTIAL_004: UWxJYzMkNSorU0tzdz09OT1wew==
+  CREDENTIAL_005: Q18yXGF7XVhEfTEjOUJwRVtrPw==
+  CREDENTIAL_006: OSpLRDhfdzwpO296YjpucztKQw==
+  CREDENTIAL_007: Q1tWJEViNXlRKWN+IS4ue0xSVA==
+  SETTING_ALLOW_ANON: dHJ1ZQ==
+  SETTING_PREVENT_ADMIN_LOGIN: dHJ1ZQ==
+  SETTING_USE_SEC: dHJ1ZQ==
+kind: Secret
+metadata:
+  creationTimestamp: "2020-07-19T16:22:25Z"
+  managedFields:
+  - apiVersion: v1
+    fieldsType: FieldsV1
+    fieldsV1:
+      f:data:
+        .: {}
+        f:CREDENTIAL_001: {}
+        f:CREDENTIAL_002: {}
+        f:CREDENTIAL_003: {}
+        f:CREDENTIAL_004: {}
+        f:CREDENTIAL_005: {}
+        f:CREDENTIAL_006: {}
+        f:CREDENTIAL_007: {}
+        f:SETTING_ALLOW_ANON: {}
+        f:SETTING_PREVENT_ADMIN_LOGIN: {}
+        f:SETTING_USE_SEC: {}
+      f:type: {}
+    manager: kubectl
+    operation: Update
+    time: "2020-07-19T16:22:25Z"
+  name: my-secret-env
+  namespace: default
+  resourceVersion: "909123"
+  selfLink: /api/v1/namespaces/default/secrets/my-secret-env
+  uid: 34ad3e90-0ada-479c-85e2-1d662351c088
+type: Opaque
+
+k run nginx --image=nginx -oyaml --dry-run=client > nginx.yaml
+
+vi ngix.yaml
+apiVersion: v1
+kind: Pod
+metadata:
+  creationTimestamp: null
+  labels:
+    run: nginx
+  name: nginx-secret
+spec:
+  containers:
+  - image: nginx
+    envFrom:                # envFrom
+    - secretRef:
+        name: my-secret
+    name: nginx
+    resources: {}
+  dnsPolicy: ClusterFirst
+  restartPolicy: Always
+status: {}
+
+k exec nginx -- env
+
+Deployment Hacking
+k create deploy nginx --image nginx
+k scale deploy nginx --replicas 3
+k get pod --show-labels
+k get rs --show-labels
+k run nginx --image=nginx --labels="app=nginx,pod-template-hash=f89759699"
+k get pods
