20458fafbb44a891ecb4d849722d125c32bce1a9
tuna <tunatore@gmail.com>
2020-08-04 18:45:11 +0200

    kubernetes summary of commands updates

--- kubernetes/ckad/ckad-summary-commands.sh
+++ kubernetes/ckad/ckad-summary-commands.sh
@@ -9,3 +9,23 @@ or
 vi ~/.vimrc
 set ts=2 sts=2 sw=2 et number
 . /~.vimrc
+
+--configMap
+kubectl create configmap app-config --from-literal=key123=value123
+
+--create secret
+kubectl create secret generic db-user-pass --from-file=./username.txt --from-file=./password.txt
+kubectl create secret generic db-user-pass --from-file=username=./username.txt --from-file=password=./password.txt
+kubectl create secret generic dev-db-secret --from-literal=username=devuser --from-literal=password='S!B\*d$zDsb='
+kubectl create secret generic my-secret --from-literal=foo=bar -o yaml --dry-run > my-secret.yaml
+k create secret generic my-secret --from-env-file=myvars.env
+kubectl get secrets
+kubectl get secret my-secret -o yaml
+
+--namespace
+kubectl get namespaces
+kubectl get namespaces --show-labels
+kubectl get pods --all-namespaces
+kubectl create ns mynamespace
+kubectl create -f pod.yaml --namespace=test
+kubectl apply -f pod.yaml --namespace=test
